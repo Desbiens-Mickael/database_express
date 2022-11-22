935c5e96df43284eed1290472fb9db8d04a9031d
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-11-22 12:20:45 +0100

    chalenge requeste post

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
@@ -11,12 +13,16 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+// get route
 app.get("/", welcome);
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getusers);
 app.get("/api/users/:id", userHandlers.getuserById);
+
+// post route
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandlers.postUser);
 
 
 app.listen(port, (err) => {
